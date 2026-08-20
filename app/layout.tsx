b786cc13b85a8c0ec48a323f06bdc5308040b142
Dolala-wsp95157 <wsp95157@gmail.com>
2026-08-20 10:39:27 +0800

    fix favicon icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,12 @@ import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
-// 🏷️ 這裡設定 LINE / FB 分享連結時會顯示的資訊
 export const metadata: Metadata = {
   title: '🏸 羽球臨打報名系統 | BBC Badminton',
   description: '快速線上預約羽球臨打時段、查看即時報名人數與程度分級！',
+  icons: {
+    icon: '/icon.png', // 指定小圖示路徑
+  },
   openGraph: {
     title: '🏸 羽球臨打報名系統 | BBC Badminton',
     description: '快速線上預約羽球臨打時段、查看即時報名人數與程度分級！',
